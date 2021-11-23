7af76bdb65912440203ea77d1a9a996a9a658abe
alexeynikitin6723 <alexey.nikitin6723@gmail.com>
2021-11-23 11:00:26 +0400

    profiles
    profiles

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const xhr = new XMLHttpRequest();
 
 let list = document.querySelector('.list__avatars');
 let listPopup = document.querySelector('.list-popup');
+let count = 0;
 
 function sendRequest(method, url) {
 	return new Promise((resolve, reject) => {
@@ -29,7 +30,7 @@ sendRequest('GET', requestURL)
 		data = data["results"];
 		for (let key in data) {
 			let name = data[key].name;
-			list.innerHTML += `<li class="item__avatars avatar-item avatar-item-list" data-name="${name.first}">
+			list.innerHTML += `<li class="item__avatars avatar-item avatar-item-list" data-name="${name.first}" data-sort="${name.title}. ${name.first} ${name.last}">
 			<div class="avatar-item__content">
 				<div class="avatar-item__image">
 					<img src="${data[key].picture.medium}" alt="medium">
@@ -37,13 +38,6 @@ sendRequest('GET', requestURL)
 				<div class="avatar-item__text">
 					<h4>${name.title}. ${name.first} ${name.last}</h4>
 				</div>
-				<div>
-					<select class="select" name="sorting" id="sorting" onchange="getSelectedIndex()">
-						<option selected value="alph-sort">Сортировка по алфавиту</option>
-						<option value="back-sort">Обратная сортировка</option>
-					</select>
-				</div>
-				<button class="sort-asc">Отсортировать</button>
 			</div>
 			</li>`
 		}
@@ -51,7 +45,7 @@ sendRequest('GET', requestURL)
 			let name = data[key].name;
 			let location = data[key].location;
 			let phone = (data[key].phone).split(' ').join('');
-			listPopup.innerHTML += `<li class="item__avatars avatar-item item-popup" data-name="${name.first}">
+			listPopup.innerHTML += `<li class="item__avatars avatar-item item-popup" data-name="${name.first}" data-sort="${name.title}. ${name.first} ${name.last}">
 			<div class="popup-content">
 				<div class="popup-header">
 					<div class="avatar-item__name">
@@ -66,11 +60,11 @@ sendRequest('GET', requestURL)
 						<img src="${data[key].picture.large}" alt="large">
 					</div>
 					<div class="avatar-item__contacts">
-						<a href="#" class="avatar-item__contacts avatar-item__contacts_map">${location.street}, ${location.city}, ${location.state}
+						<a href="#" class="avatar-item__contact avatar-item__contact_map">${location.street}, ${location.city}, ${location.state}
 							${location.postcode}</a>
-						<a href="tel:${phone}" class="avatar-item__contacts avatar-item__contacts_phone">${data[key].phone}</a>
+						<a href="tel:${phone}" class="avatar-item__contact avatar-item__contact_phone">${data[key].phone}</a>
 						<a href="mailto:${data[key].email}"
-							class="avatar-item__contacts avatar-item__contacts_email">${data[key].email}</a>
+							class="avatar-item__contact avatar-item__contact_email">${data[key].email}</a>
 					</div>
 				</div>
 			</div>
@@ -80,8 +74,9 @@ sendRequest('GET', requestURL)
 	.catch(err => console.log(err));
 
 function getSelectedIndex() {
-	return document.getElementById('sorting').selectedIndex;
+	return document.querySelector('.select__current').innerText;
 }
+
 
 document.body.addEventListener('click', event => {
 	if (event.target.classList.contains('sort-asc')) {
@@ -89,23 +84,23 @@ document.body.addEventListener('click', event => {
 	}
 });
 function mySort() {
-	let nav = document.querySelector('#nav');
-	if (getSelectedIndex() === 0) {
-		for (let i = 0; i < nav.children.length; i++) {
-			for (let j = i; j < nav.children.length; j++) {
-				if (nav.children[i].getAttribute('data-name') > nav.children[j].getAttribute('data-name')) {
-					replaceNode = nav.replaceChild(nav.children[j], nav.children[i]);
-					insertAfter(replaceNode, nav.children[i]);
+	//let list = document.querySelector('#list');
+	if (getSelectedIndex() === 'Сортировка по алфавиту') {
+		for (let i = 0; i < list.children.length; i++) {
+			for (let j = i; j < list.children.length; j++) {
+				if (list.children[i].getAttribute('data-name') > list.children[j].getAttribute('data-name')) {
+					replaceNode = list.replaceChild(list.children[j], list.children[i]);
+					insertAfter(replaceNode, list.children[i]);
 				}
 			}
 		}
 	}
-	if (getSelectedIndex() === 1) {
-		for (let i = 0; i < nav.children.length; i++) {
-			for (let j = i; j < nav.children.length; j++) {
-				if (nav.children[i].getAttribute('data-name') < nav.children[j].getAttribute('data-name')) {
-					replaceNode = nav.replaceChild(nav.children[j], nav.children[i]);
-					insertAfter(replaceNode, nav.children[i]);
+	if (getSelectedIndex() === 'Обратная сортировка') {
+		for (let i = 0; i < list.children.length; i++) {
+			for (let j = i; j < list.children.length; j++) {
+				if (list.children[i].getAttribute('data-name') < list.children[j].getAttribute('data-name')) {
+					replaceNode = list.replaceChild(list.children[j], list.children[i]);
+					insertAfter(replaceNode, list.children[i]);
 				}
 			}
 		}
@@ -121,17 +116,61 @@ let popupClose = document.querySelectorAll('.close');
 document.body.addEventListener('click', event => {
 	if (event.target.classList.contains('close')) {
 		popup.style.display = "none";
+		list.children[count].querySelector('.avatar-item__image').classList.remove('triangle');
 	}
 });
 list.addEventListener('click', function (event) {
-	if (event.target.closest('.avatar-item__content') && !event.target.classList.contains('sort-asc') && !event.target.classList.contains('select')) {
+	if (event.target.closest('.avatar-item__content')) {
 		popup.style.display = "block";
-		console.log(list.children[2].getAttribute('data-name'));
+		outputOnDisplay(event);
 	}
 });
 listPopup.addEventListener('click', function (event) {
 	if (!event.target.closest('.popup-content')) {
 		popup.style.display = "none";
+		list.children[count].querySelector('.avatar-item__image').classList.remove('triangle');
 	}
 });
+
+function outputOnDisplay(event) {
+	for (let i = 0; i < listPopup.children.length; i++) {
+		if (event.target.closest('li').getAttribute('data-sort') == listPopup.children[i].getAttribute('data-sort')) {
+			listPopup.children[i].style.display = "block";
+		}
+		else {
+			listPopup.children[i].style.display = "none";
+		}
+		if (event.target.closest('li').getAttribute('data-sort') == list.children[i].getAttribute('data-sort')) {
+			list.children[i].querySelector('.avatar-item__image').classList.add('triangle');
+			count = i;
+		}
+	}
+}
+
+////////////////////////////////////////////////////////
+
+let select = function () {
+	let selectHeader = document.querySelectorAll('.select__header');
+	let selectItem = document.querySelectorAll('.select__item');
+
+	selectHeader.forEach((event) => {
+		event.addEventListener('click', selectToggle);
+	});
+
+	selectItem.forEach((event) => {
+		event.addEventListener('click', selectChoose);
+	});
+
+	function selectToggle() {
+		this.parentElement.classList.toggle('is-active');
+	}
+	function selectChoose() {
+		let text = this.innerText;
+		let select = this.closest('.select');
+		let currentText = select.querySelector('.select__current');
+		currentText.innerText = text;
+		select.classList.remove('is-active');
+	}
+};
+select();
 
